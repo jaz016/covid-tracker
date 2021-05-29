bbf2f212809f3bcfd152dc23d96b528492ab0c9b
Jasper Bautista <xjet16@gmail.com>
2021-05-29 14:43:02 +0800

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
+import { format } from 'date-fns'
 import React from 'react'
 
 
 const Footer = () => {
 	return (
-		<footer className='mt-5 text-center'>
-			<p>Copyright &copy; 2021 COVID Tracker</p>
+		<footer className='mt-5 mb-4 text-center'>
+			<p>Powered by <a href='https://reactjs.org/'><strong>React</strong></a> and <a href='https://covid19api.com/'><strong>covid19api.com</strong></a></p>
+			<p>Copyright &copy; {format(Date.now(), 'yyyy')} COVID Tracker</p>
 		</footer>
 	)
 }
